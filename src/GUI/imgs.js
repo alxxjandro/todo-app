c172be98c3836f21a02b8c60ade7a4790cffb177
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-01-08 17:45:52 -0600

    still working on the sidebar design

--- src/GUI/imgs.js
+++ src/GUI/imgs.js
@@ -3,13 +3,15 @@ import calendar from "../icons/calendar.svg";
 import rightArrow from "../icons/rightArrow.svg";
 import upcomingCalendar from "../icons/upcomingCalendar.svg";
 import dashboard from "../icons/dashboard.svg";
+import star from "../icons/star.svg";
 
 const images = {
     addButton,
     calendar,
     rightArrow,
     upcomingCalendar,
-    dashboard
+    dashboard,
+    star
 }
 
 export default images;
--- src/GUI/sidebar.js
+++ src/GUI/sidebar.js
@@ -6,6 +6,8 @@ const sideBar = function (){
     const container = document.createElement("div");
     container.className = "sideBar";
 
+    const taskText = createElem("h3",["Task"],["sideBarText"],container);
+
     //button for toggling the menu
     const menuBtnContainer = document.createElement("div");
     const toggleMenuBtn = document.createElement("button");
@@ -16,23 +18,30 @@ const sideBar = function (){
 
     menuBtnContainer.appendChild(toggleMenuBtn);
     menuBtnContainer.appendChild(toggleMenuBtn);
-    container.appendChild(menuBtnContainer);
+    taskText.appendChild(menuBtnContainer);
 
     //sideBar buttons
     const dashboardBtn = createElem("button",["Dashboard"],["dashboardBtn"],container);
     const dashboardImg = document.createElement("img");
     dashboardImg.src = images.dashboard;
 
-    const todayBtn = createElem("button",["Today"],["todayBtn"],container);
+    const todayBtn = createElem("button",["Upcoming"],["todayBtn"],container);
     const todayImg = document.createElement("img");
     todayImg.src = images.calendar;
 
+    const importantBtn = createElem("button",["Important"],["importantBtn"],container);
+    const importantImg = document.createElement("img");
+    importantImg.src = images.star;
+
+
     dashboardBtn.appendChild(dashboardImg);
     todayBtn.appendChild(todayImg);
+    importantBtn.appendChild(importantImg);
+
+    const projectsText = createElem("h3",["Projects"],["sideBarText"],container);
 
     //making the button functional
     toggleMenuBtn.addEventListener("click",() =>{
-
         container.classList.toggle("collapsed");
     })
 

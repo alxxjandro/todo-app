3799941a408de940f44a4817c50c953bdccd95d1
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-01-12 14:35:33 -0600

    working on the design of the adding project menu

--- src/GUI/createProjectMenu.js
+++ src/GUI/createProjectMenu.js
@@ -3,14 +3,38 @@ import "../css/createProject.css"
 
 const createProject = function (){
     const contentDiv = Object.assign(document.querySelector(".content"),{ innerHTML : "" });;
-    const textTitle = createElem("h2",["Create a new project"],["newProjectTitle"],contentDiv);
+    const container = Object.assign(document.createElement("div"),{ className : "newProjectContainer"});
+
+    const textTitle = createElem("h2",["Create a new project"],["newProjectTitle"],container);
     const projectForm = utils.createInput(
         "Project Name",
         "text",
         "projectName",
         "projectName",
         "A really cool one!",
-        contentDiv);
+        container
+    );
+
+    const btnsContainer = container.appendChild(document.createElement("div"));
+    btnsContainer.classList = "btnsContainer";
+
+    const cancelBtn = btnsContainer.appendChild(
+        Object.assign(document.createElement("button"),{
+            innerText : "Cancel",
+            className : "cancelBtnInput"
+        })
+    )
+
+    const submitBtn = btnsContainer.appendChild(
+        Object.assign(document.createElement("button"),{
+            innerText : "Create",
+            className : "npBtnInput",
+            type : "input"
+        })
+    )
+
+    contentDiv.appendChild(container);
+
 };
 
 export default createProject;
